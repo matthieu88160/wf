f2d6c624091a950f90532976f085d88dae16a891
matthieu88160 <matthieu.vallance@cscfa.fr>
2018-01-06 21:44:14 +0100

    Correct PHP exercice 1

---
+++ PHP/Exercice1/correction.php
@@ -0,0 +1,28 @@
+<?php
+
+$booleanTrue = true;
+$booleanFalse = false;
+
+$int = 12;
+
+$float = 13.4;
+
+$integer = 4;
+
+$double = 3e5;
+
+$string = "hello world";
+
+$array = ['hello', 'world'];
+
+$doubleDimenssionArray = [
+    [3, 5],
+    [2, 8]
+];
+
+$associativeArray = [
+    'x' => 3,
+    'y' => 14
+];
+
+$null = null;
