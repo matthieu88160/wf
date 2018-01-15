40a6f2be191df0c4f3499e5210a356d83a61b435
matthieu88160 <matthieu.vallance@cscfa.fr>
2018-01-15 16:38:38 +0100

    Create TagBeSill exercise 4

--- Server/Exercice/src/addpage.php
+++ Server/Exercice/src/addpage.php
@@ -2,17 +2,51 @@
 require dirname(__DIR__).'/vendor/autoload.php';
 use Ramsey\Uuid\Uuid;
 
-function generateToken() : string {
+if (session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+
+function generateToken(&$uuid, &$tag) : string {
     $cipher = "aes-256-gcm";
     $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($cipher));
-    $requestId = Uuid::uuid1();
+    $uuid = Uuid::uuid1();
     
-    return openssl_encrypt($requestId, $cipher, gethostname(), OPENSSL_ZERO_PADDING, $iv, $tag);
+    return openssl_encrypt($uuid, $cipher, gethostname(), OPENSSL_ZERO_PADDING, $iv, $tag);
+}
+
+function tokenKeepAlive() {
+    $current = new DateTime();
+    $alive = [];
+    foreach ($_SESSION['csrf_tokens'] as $uuid => $infos) {
+        if ($infos['valid_period'] > $current) {
+            $alive[$uuid] = $_SESSION['csrf_tokens'][$uuid];
+        }
+    }
+    
+    $_SESSION['csrf_tokens'] = $alive;
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $token = generateToken();
+    $token = generateToken($uuid, $tag);
+    $tokenLimit = 120;
+    
+    if (!isset($_SESSION['csrf_tokens'])) {
+        $_SESSION['csrf_tokens'] = [];
+    } else if (count($_SESSION['csrf_tokens']) > $tokenLimit) {
+        $_SESSION['csrf_tokens'] = array_slice($_SESSION['csrf_tokens'], count($_SESSION['csrf_tokens'])-$tokenLimit, $tokenLimit, true);
+    }
+    
+    tokenKeepAlive();
+    
+    $_SESSION['csrf_tokens'][(string)$uuid] = [
+        'uuid' => (string)$uuid,
+        'tag' => $tag,
+        'valid_period' => new DateTime('now +1hour')
+    ];
 }
+
+session_write_close();
+
 ?>
 <!DOCTYPE html>
 <html>
