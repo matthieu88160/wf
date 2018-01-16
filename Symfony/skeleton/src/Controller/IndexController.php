4f43a71bb7d731a0c14eacd5330cdb8eaacb52cc
matthieu88160 <matthieu.vallance@cscfa.fr>
2018-01-16 14:04:57 +0100

    Correct symfony exercise 3

--- Symfony/skeleton/src/Controller/IndexController.php
+++ Symfony/skeleton/src/Controller/IndexController.php
@@ -2,12 +2,18 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
 class IndexController
 {
+    private $twig;
+    
+    public function __construct(Environment $twig) {
+        $this->twig = $twig;
+    }
+
     public function indexAction()
     {
-        return new Response('<!DOCTYPE html><html><head></head><body>hello world</body></html>');
+        return new Response($this->twig->render('index/index.html.twig'));
     }
 }
-
