65a8a73ced6bf81d839304a5caf7a7d1c18883c1
matthieu88160 <matthieu.vallance@cscfa.fr>
2018-01-18 10:20:50 +0100

    Correct SF exercise 5

---
+++ Symfony/skeleton/src/Entity/Project.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProjectRepository")
+ * @ORM\Table(name="project",indexes={@ORM\Index(name="project_name_idx", columns={"name"})})
+ */
+class Project
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer", options={"unsigned":true})
+     */
+    private $id;
+    
+    /**
+     * @ORM\Column(type="string", length=255, unique=true, nullable=false)
+     */
+    private $name;
+    
+    /**
+     * @ORM\Column(type="text", unique=false, nullable=false)
+     */
+    private $description;
+    
+    /**
+     * @ORM\Column(type="datetimetz", nullable=false)
+     */
+    private $createdAt;
+    
+    /**
+     * @ORM\Column(type="boolean", nullable=false, options={"default":false})
+     */
+    private $published = false;
+    
+    /**
+     * @ORM\Column(type="boolean", nullable=false, options={"default":false})
+     */
+    private $deleted = false;
+    
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    public function getId() : int
+    {
+        return $this->id;
+    }
+
+    public function getName() : ?string
+    {
+        return $this->name;
+    }
+
+    public function getDescription() : ?string
+    {
+        return $this->description;
+    }
+
+    public function getCreatedAt() : \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function isPublished() : bool
+    {
+        if ($this->isDeleted()) {
+            return false;
+        }
+
+        return $this->published;
+    }
+
+    public function isDeleted() : bool
+    {
+        return $this->deleted;
+    }
+
+    public function setName(string $name) : Project
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function setDescription(string $description) : Project
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function setPublished(bool $published) : Project
+    {
+        $this->published = $published;
+        return $this;
+    }
+
+    public function setDeleted(bool $deleted) : Project
+    {
+        $this->deleted = $deleted;
+        return $this;
+    }
+
+}
---
+++ Symfony/skeleton/src/Migrations/Version20180117113734.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180117113734 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE project (id INT UNSIGNED AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, created_at DATETIME NOT NULL, published TINYINT(1) DEFAULT \'0\' NOT NULL, deleted TINYINT(1) DEFAULT \'0\' NOT NULL, UNIQUE INDEX UNIQ_2FB3D0EE5E237E06 (name), INDEX project_name_idx (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE project');
+    }
+}
