8be838866959e6f7d8c7c03a238386900b9777ee
matthieu88160 <matthieu.vallance@cscfa.fr>
2018-01-22 10:27:25 +0100

    Add monolog, add office access control

--- Symfony/skeleton/config/bundles.php
+++ Symfony/skeleton/config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
